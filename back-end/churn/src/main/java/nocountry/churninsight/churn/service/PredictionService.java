32b460516fdd0b749a8803225b8b81356b2268ca
Augusto <augustoramos067@gmail.com>
2025-12-17 16:15:58 -0300

    ajuste no service

--- back-end/churn/src/main/java/nocountry/churninsight/churn/service/PredictionService.java
+++ back-end/churn/src/main/java/nocountry/churninsight/churn/service/PredictionService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PredictionService {
     public PredictDTO predict(ChurnDataDTO data) {
-        // Valor de retorno provisório enquanto o modelo de IA não é integrado.
+        // Valor de retorno provisório enquanto o modelo não é integrado.
         // A lógica abaixo é apenas um exemplo e será substituída.
         if (data.getValorMensal() > 100) {
             return new PredictDTO("Vai cancelar", 0.8);
