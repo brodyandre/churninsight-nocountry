cb432abecc94e7aad60f5d124e519454806d970f
Beatriz Christie <beatrizchristie@yahoo.com.br>
2025-12-18 16:20:24 -0300

    Adiciona erro de validação no GlobalExceptionHandler

--- back-end/churn/src/main/java/nocountry/churninsight/churn/exception/GlobalExceptionHandler.java
+++ back-end/churn/src/main/java/nocountry/churninsight/churn/exception/GlobalExceptionHandler.java
@@ -1,14 +1,42 @@
 package nocountry.churninsight.churn.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // Status 400 - Erro de Validação
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest req) {
+
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fieldError -> fieldError.getDefaultMessage() != null
+                                ? fieldError.getDefaultMessage()
+                                : "Erro sem mensagem definida.",
+                        (existingValue, newValue) -> existingValue));
+
+        return buildResponse(HttpStatus.valueOf(status.value()),
+                "Erro de validação dos campos.",
+                errors);
+    }
 
     private ResponseEntity<Object> buildResponse(HttpStatus status, String message, Object details) {
         ErrorResponse error = new ErrorResponse(
