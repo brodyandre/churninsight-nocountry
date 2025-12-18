e9965036b1e3b5a0d5d585a425670fcc77573ea5
Augusto <augustoramos067@gmail.com>
2025-12-17 22:00:03 -0300

    adição logs

--- back-end/churn/src/main/java/nocountry/churninsight/churn/controller/ChurnController.java
+++ back-end/churn/src/main/java/nocountry/churninsight/churn/controller/ChurnController.java
@@ -1,18 +1,37 @@
 package nocountry.churninsight.churn.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import nocountry.churninsight.churn.dto.ChurnDataDTO;
+import nocountry.churninsight.churn.dto.PredictDTO;
+import nocountry.churninsight.churn.service.PredictionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class ChurnController{
+@RequestMapping("/churn")
+public class ChurnController {
 
-    @GetMapping(value = "/example",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public String test(){
-        return "teste";
+    private static final Logger logger = LoggerFactory.getLogger(ChurnController.class);
+
+    @Autowired
+    private PredictionService predictionService;
+
+    @PostMapping("/predict")
+    public ResponseEntity<PredictDTO> predictChurn(@RequestBody ChurnDataDTO data) {
+        logger.info("Recebida requisição para previsão de churn.");
+        
+        try {
+            PredictDTO prediction = predictionService.predict(data);
+            return ResponseEntity.ok(prediction);
+        } catch (Exception e) {
+            logger.error("Erro ao processar a requisição de previsão de churn.", e);
+            // Retorna um erro 500 (Internal Server Error) se algo der errado no serviço
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
