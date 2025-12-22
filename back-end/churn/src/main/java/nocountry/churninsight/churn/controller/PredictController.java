3d07ce83ffc2b55f5367a6ca308f3b010b2f4a20
Beatriz Christie <beatrizchristie@yahoo.com.br>
2025-12-22 11:34:31 -0300

    fix: Substitui PredictDTO por ChurnDataDTO e injeta PredictionService no PredictController

--- back-end/churn/src/main/java/nocountry/churninsight/churn/controller/PredictController.java
+++ back-end/churn/src/main/java/nocountry/churninsight/churn/controller/PredictController.java
@@ -1,7 +1,10 @@
 package nocountry.churninsight.churn.controller;
 
 import jakarta.validation.Valid;
+import nocountry.churninsight.churn.dto.ChurnDataDTO;
 import nocountry.churninsight.churn.dto.PredictDTO;
+import nocountry.churninsight.churn.service.PredictionService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class PredictController {
 
+    private final PredictionService predictionService;
+
+    public PredictController(PredictionService predictionService) {
+        this.predictionService = predictionService;
+    }
+
     @PostMapping("/predict")
-    public ResponseEntity<PredictDTO> predict (@Valid @RequestBody PredictDTO previsao ){
-        predict(previsao);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<PredictDTO> predict (@Valid @RequestBody ChurnDataDTO previsao){
+        PredictDTO resultado = predictionService.predict(previsao);
+        return ResponseEntity.ok(resultado);
     }
 
 }
